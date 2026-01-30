0fb712315a9913debc835d4305a7f2e35a5a6b75
yhh1056 <hyeonho9995@gmail.com>
2026-01-30 17:01:37 +0900

    환경 분리

--- src/test/java/com/camping/legacy/repository/ReservationRepositoryTest.java
+++ src/test/java/com/camping/legacy/repository/ReservationRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDate;
@@ -14,6 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @TestPropertySource(properties = "spring.sql.init.mode=never")
+@ActiveProfiles("test")
 class ReservationRepositoryTest {
 
     @Autowired
